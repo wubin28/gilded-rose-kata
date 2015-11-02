51567bdc45c6332ff29c855aca785d8ce9da69aa
snake_86132D0222 <snake@cyber-dojo.org>
2015-11-02 11:51:18 +0000

    5

--- sandbox/cyber-dojo.sh
+++ sandbox/cyber-dojo.sh
@@ -4,3 +4,5 @@ javac -Xlint:unchecked -Xlint:deprecation -cp $CLASSES  *.java
 if [ $? -eq 0 ]; then
   java -cp $CLASSES org.junit.runner.JUnitCore `ls -1 *Test*.class | grep -v '\\$' | sed 's/\(.*\)\..*/\1/'`
 fi
+
+java -cp $CLASSES TexttestFixture
