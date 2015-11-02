8e5b3bd4b5aab3406dd3f8c9b30701295184f95d
snake_86132D0222 <snake@cyber-dojo.org>
2015-11-02 11:49:39 +0000

    3

---
+++ sandbox/GildedRoseTest.java
@@ -0,0 +1,14 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class GildedRoseTest {
+
+    @Test
+    public void foo() {
+        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("foo", app.items[0].name);
+    }
+
+}
