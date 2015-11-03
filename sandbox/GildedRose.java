7e868f4e28020773e163669cc13424b826d10e56
snake_86132D0222 <snake@cyber-dojo.org>
2015-11-03 14:21:16 +0000

    27

--- sandbox/GildedRose.java
+++ sandbox/GildedRose.java
@@ -47,7 +47,7 @@ class GildedRose {
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        items[i].quality = 0;
                     }
                 } else {
                     if (items[i].quality < 50) {
