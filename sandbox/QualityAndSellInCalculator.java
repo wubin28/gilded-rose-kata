d555ea02349a9d745693fa2d79b2375485976399
snake_86132D0222 <snake@cyber-dojo.org>
2015-11-03 17:40:18 +0000

    37

--- sandbox/QualityAndSellInCalculator.java
+++ sandbox/QualityAndSellInCalculator.java
@@ -1,12 +1,21 @@
 public enum QualityAndSellInCalculator {
     AgedBrie {
+        @Override
+        public void updateQualityAndSellIn(Item item) {
+        }
     },
     Sulfuras {
+        @Override
+        public void updateQualityAndSellIn(Item item) {
+        }
     },
     BackStagePasses {
+        @Override
+        public void updateQualityAndSellIn(Item item) {
+        }
     }
     ;
     
-    public abstract String updateQualityAndSellIn(Item item) {
+    public abstract void updateQualityAndSellIn(Item item) {
     }
 }
