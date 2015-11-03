d918d295fbf95a4f1966d9f05030f81350c15272
snake_86132D0222 <snake@cyber-dojo.org>
2015-11-03 17:45:49 +0000

    39

--- sandbox/GildedRose.java
+++ sandbox/GildedRose.java
@@ -7,58 +7,61 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            QualityAndSellInCalculator.valueOf(items[i].name).updateQualityAndSellIn(items[i]);
+            try {
+                QualityAndSellInCalculator.valueOf(items[i].name).updateQualityAndSellIn(items[i]);
+            } catch (IllegalArgumentException illegalArgumentException) {
+                if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) continue;
 
-            if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) continue;
+                if (items[i].name.equals("Aged Brie")) {
+                    if (items[i].quality < 50) items[i].quality += 1;
 
-            if (items[i].name.equals("Aged Brie")) {
-                if (items[i].quality < 50) items[i].quality += 1;
+                    items[i].sellIn -= 1;
 
-                items[i].sellIn -= 1;
-
-                if (items[i].sellIn < 0 && items[i].quality < 50) {
-                    items[i].quality += 1;
+                    if (items[i].sellIn < 0 && items[i].quality < 50) {
+                        items[i].quality += 1;
+                    }
+                    continue;
                 }
-                continue;
-            }
 
-            if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality < 50) {
-                    items[i].quality += 1;
+                if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (items[i].quality < 50) {
+                        items[i].quality += 1;
 
-                    if (items[i].sellIn < 11) {
-                        if (items[i].quality < 50) {
-                            items[i].quality += 1;
+                        if (items[i].sellIn < 11) {
+                            if (items[i].quality < 50) {
+                                items[i].quality += 1;
+                            }
+                        }
+
+                        if (items[i].sellIn < 6) {
+                            if (items[i].quality < 50) {
+                                items[i].quality += 1;
+                            }
                         }
                     }
 
-                    if (items[i].sellIn < 6) {
-                        if (items[i].quality < 50) {
-                            items[i].quality += 1;
-                        }
+                    items[i].sellIn -= 1;
+
+                    if (items[i].sellIn < 0) {
+                        items[i].quality = 0;
                     }
+
+                    continue;
+                }
+
+                if (items[i].quality > 0) {
+                    items[i].quality -= 1;
                 }
 
                 items[i].sellIn -= 1;
 
                 if (items[i].sellIn < 0) {
-                    items[i].quality = 0;
-                }
-
-                continue;
-            }
-
-            if (items[i].quality > 0) {
-                items[i].quality -= 1;
-            }
-
-            items[i].sellIn -= 1;
-
-            if (items[i].sellIn < 0) {
-                if (items[i].quality > 0) {
-                    items[i].quality -= 1;
+                    if (items[i].quality > 0) {
+                        items[i].quality -= 1;
+                    }
                 }
             }
+
         }
     }
 }
