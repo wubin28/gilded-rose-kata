72c73f709a7b6254ddf0987236acefd8f271b5bc
snake_86132D0222 <snake@cyber-dojo.org>
2015-11-03 18:16:26 +0000

    52

--- sandbox/QualityAndSellInCalculator.java
+++ sandbox/QualityAndSellInCalculator.java
@@ -22,26 +22,26 @@ public enum QualityAndSellInCalculator {
         @Override
         public void updateQualityAndSellIn(Item item) {
             System.out.println(">>>In Backstage");
-            if (items[i].quality < 50) {
-                items[i].quality += 1;
+            if (item.quality < 50) {
+                item.quality += 1;
 
-                if (items[i].sellIn < 11) {
-                    if (items[i].quality < 50) {
-                        items[i].quality += 1;
+                if (item.sellIn < 11) {
+                    if (item.quality < 50) {
+                        item.quality += 1;
                     }
                 }
 
-                if (items[i].sellIn < 6) {
-                    if (items[i].quality < 50) {
-                        items[i].quality += 1;
+                if (item.sellIn < 6) {
+                    if (item.quality < 50) {
+                        item.quality += 1;
                     }
                 }
             }
 
-            items[i].sellIn -= 1;
+            item.sellIn -= 1;
 
-            if (items[i].sellIn < 0) {
-                items[i].quality = 0;
+            if (item.sellIn < 0) {
+                item.quality = 0;
             }
 
         }
