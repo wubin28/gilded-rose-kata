d99406cd20aaff1323f01ff278fbd329bfea5a33
snake_86132D0222 <snake@cyber-dojo.org>
2015-11-03 13:12:47 +0000

    7

--- sandbox/GildedRoseTest.java
+++ sandbox/GildedRoseTest.java
@@ -11,10 +11,15 @@ public class GildedRoseTest {
         assertEquals("foo", app.items[0].name);
     }
 
-    // TODO: both_sellIn_and_quality_of_normal_item_should_be_lowered_at_the_end_of_each_day
+    // TODO: quality_of_normal_item_should_be_lowered_at_the_end_of_each_day
     // TODO: quality_of_normal_item_should_not_be_lowered_to_be_negative
     // TODO: quality_of_aged_brie_should_increase_at_the_end_of_each_day
-    // TODO: quality_of_aged_brie_should_not_increase_to_be_more_than_50
+    // TODO: quality_of_aged_brie_should_increase_up_to_50
     // TODO: quality_of_backstage_passes_should_increase_by_1_if_sellIn_is_more_than_10
+    // TODO: quality_of_backstage_passes_should_increase_by_2_if_sellIn_is_more_than_5_and_less_than_11
+    // TODO: quality_of_backstage_passes_should_increase_by_3_if_sellIn_is_more_than_0_and_less_than_6
+    // TODO: quality_of_backstage_passes_should_increase_up_to_50
+    // TODO: sellIn_of_sulfuras_should_never_be_changed
+    // TODO: sellIn_of_item_other_than_sulfuras_should_be_lowered_at_the_end_of_each_day
 
 }
