84bbf0e11ec34be042cb165bd1cd10655638b397
snake_86132D0222 <snake@cyber-dojo.org>
2015-11-03 14:26:05 +0000

    29

--- sandbox/GildedRose.java
+++ sandbox/GildedRose.java
@@ -7,12 +7,12 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
+            if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) return;
+
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality -= 1;
-                    }
+                    items[i].quality -= 1;
                 }
             } else {
                 if (items[i].quality < 50) {
@@ -34,17 +34,13 @@ class GildedRose {
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn -= 1;
-            }
+            items[i].sellIn -= 1;
 
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality -= 1;
-                            }
+                            items[i].quality -= 1;
                         }
                     } else {
                         items[i].quality = 0;
