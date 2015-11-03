c2fdb8f76532ac1036a9f544b105b7afd833db0c
snake_86132D0222 <snake@cyber-dojo.org>
2015-11-03 13:01:49 +0000

    6

--- sandbox/GildedRoseTest.java
+++ sandbox/GildedRoseTest.java
@@ -11,4 +11,10 @@ public class GildedRoseTest {
         assertEquals("foo", app.items[0].name);
     }
 
+    // TODO: both_sellIn_and_quality_of_normal_item_should_be_lowered_at_the_end_of_each_day
+    // TODO: quality_of_normal_item_should_not_be_lowered_to_be_negative
+    // TODO: quality_of_aged_brie_should_increase_at_the_end_of_each_day
+    // TODO: quality_of_aged_brie_should_not_increase_to_be_more_than_50
+    // TODO: quality_of_backstage_passes_should_increase_by_1_if_sellIn_is_more_than_10
+
 }
