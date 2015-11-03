fcb40a53f0c47f497517475e5e4b99a07456df5d
snake_86132D0222 <snake@cyber-dojo.org>
2015-11-03 14:50:37 +0000

    33

--- sandbox/GildedRose.java
+++ sandbox/GildedRose.java
@@ -18,11 +18,7 @@ class GildedRose {
                 continue;
             }
 
-            if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    items[i].quality -= 1;
-                }
-            } else {
+            if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality < 50) {
                     items[i].quality += 1;
 
@@ -38,17 +34,25 @@ class GildedRose {
                         }
                     }
                 }
+
+                items[i].sellIn -= 1;
+
+                if (items[i].sellIn < 0) {
+                    items[i].quality = 0;
+                }
+
+                continue;
+            }
+
+            if (items[i].quality > 0) {
+                items[i].quality -= 1;
             }
 
             items[i].sellIn -= 1;
 
             if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (items[i].quality > 0) {
-                        items[i].quality -= 1;
-                    }
-                } else {
-                    items[i].quality = 0;
+                if (items[i].quality > 0) {
+                    items[i].quality -= 1;
                 }
             }
         }
