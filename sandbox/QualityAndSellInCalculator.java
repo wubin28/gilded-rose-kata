704c9bf1799b7cfb0ad79970cdb33e0995d4716e
snake_86132D0222 <snake@cyber-dojo.org>
2015-11-03 18:08:28 +0000

    48

--- sandbox/QualityAndSellInCalculator.java
+++ sandbox/QualityAndSellInCalculator.java
@@ -1,42 +1,24 @@
 public enum QualityAndSellInCalculator {
-    AgedBrie("Aged Brie") {
+    AgedBrie {
         @Override
         public void updateQualityAndSellIn(Item item) {
             System.out.println(">>>In Aged Brie.");
         }
-        @Override
-        public String toString() {
-            return super.displayName;
-        }
     },
-    Sulfuras("Sulfuras, Hand of Ragnaros") {
+    SulfurasHandOfRagnaros {
         @Override
         public void updateQualityAndSellIn(Item item) {
             System.out.println(">>>In Sulfuras");
         }
-        @Override
-        public String toString() {
-            return super.displayName;
-        }
     },
-    BackStagePasses("Backstage passes to a TAFKAL80ETC concert") {
+    BackStagePassesToATAFKAL80Concert {
         @Override
         public void updateQualityAndSellIn(Item item) {
             System.out.println(">>>In Backstage");
         }
-        @Override
-        public String toString() {
-            return super.displayName;
-        }
     }
     ;
 
-    protected String displayName;
-
-    QualityAndSellInCalculator(String displayName) {
-        this.displayName = displayName;
-    }
-    
     public abstract void updateQualityAndSellIn(Item item);
 
 }
