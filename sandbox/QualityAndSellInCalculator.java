9ac74793cfae8db00f3ae85a2062c17c437d25ef
snake_86132D0222 <snake@cyber-dojo.org>
2015-11-03 17:54:45 +0000

    43

--- sandbox/QualityAndSellInCalculator.java
+++ sandbox/QualityAndSellInCalculator.java
@@ -2,16 +2,19 @@ public enum QualityAndSellInCalculator {
     AgedBrie("Aged Brie") {
         @Override
         public void updateQualityAndSellIn(Item item) {
+            System.out.println("===In Aged Brie.");
         }
     },
     Sulfuras("Sulfuras, Hand of Ragnaros") {
         @Override
         public void updateQualityAndSellIn(Item item) {
+            System.out.println("===In Sulfuras");
         }
     },
     BackStagePasses("Backstage passes to a TAFKAL80ETC concert") {
         @Override
         public void updateQualityAndSellIn(Item item) {
+            System.out.println("===In Backstage");
         }
     }
     ;
