0a2f02a236eeee5a3dce5fc5b863a8b862f037bd
snake_86132D0222 <snake@cyber-dojo.org>
2015-11-04 21:09:59 +0000

    67

--- sandbox/QualityAndSellInCalculator.java
+++ sandbox/QualityAndSellInCalculator.java
@@ -1,11 +1,12 @@
 public enum QualityAndSellInCalculator {
     Aged_Brie {
         @Override
-        public void updateQualityAndSellIn(Item item) {
+        public void updateQualityBeforeDecreasingSellIn(Item item) {
             if (item.quality < 50) item.quality += 1;
+        }
 
-            item.sellIn -= 1;
-
+        @Override
+        public void updateQualityAfterDecreasingSellIn(Item item) {
             if (item.sellIn < 0 && item.quality < 50) {
                 item.quality += 1;
             }
@@ -13,12 +14,16 @@ public enum QualityAndSellInCalculator {
     },
     Sulfuras_Hand_Of_Ragnaros {
         @Override
-        public void updateQualityAndSellIn(Item item) {
+        public void updateQualityBeforeDecreasingSellIn(Item item) {
+        }
+
+        @Override
+        public void updateQualityAfterDecreasingSellIn(Item item) {
         }
     },
     Backstage_Passes_To_A_TAFKAL80ETC_Concert {
         @Override
-        public void updateQualityAndSellIn(Item item) {
+        public void updateQualityBeforeDecreasingSellIn(Item item) {
             if (item.quality < 50) {
                 item.quality += 1;
 
@@ -34,25 +39,26 @@ public enum QualityAndSellInCalculator {
                     }
                 }
             }
+        }
 
-            item.sellIn -= 1;
-
+        @Override
+        public void updateQualityAfterDecreasingSellIn(Item item) {
             if (item.sellIn < 0) {
                 item.quality = 0;
             }
-
         }
     },
     Conjured_Mana_Cake {
         @Override
-        public void updateQualityAndSellIn(Item item) {
+        public void updateQualityBeforeDecreasingSellIn(Item item) {
             if (item.quality > 0) {
                 item.quality -= 2;
                 if (item.quality < 0) item.quality = 0;
             }
+        }
 
-            item.sellIn -= 1;
-
+        @Override
+        public void updateQualityAfterDecreasingSellIn(Item item) {
             if (item.sellIn < 0) {
                 if (item.quality > 0) {
                     item.quality -= 2;
@@ -64,6 +70,16 @@ public enum QualityAndSellInCalculator {
     ;
 
     public void updateQualityAndSellIn(Item item){
+        updateQualityBeforeDecreasingSellIn(Item item);
+        decreaseSellIn(Item item);
+        updateQualityAfterDecreasingSellIn(Item item);
+    }
+    
+    protected abstract void updateQualityBeforeDecreasingSellIn(Item item); 
+
+    protected void decreaseSellIn(Item item) {
+        item.sellIn -= 1;
     }
 
+    protected abstract void updateQualityAfterDecreasingSellIn(Item item); 
 }
